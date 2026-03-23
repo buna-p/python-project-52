b826510bd7997dbce1a6c0f29a44da76106921ff
Natali <burdinsckaya@ya.ru>
2026-03-23 17:11:55 +0300

    add render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['localhost', '.onrender.com',]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') + ['webserver', 'localhost', '.onrender.com',]
 
 # Application definition
 
